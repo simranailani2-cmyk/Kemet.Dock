229744c893ac490cce373bf56fd78392275a15eb
simranailani2-cmyk <294170612+simranailani2-cmyk@users.noreply.github.com>
2026-07-25 14:27:47 +0000

    Update Egyptian UI Doodles for Main Page and Sidebar
    Removed the pyramid doodle from the main page and replaced it with a side-by-side Egyptian Cat and Pharaoh aligned strictly to the far left using a 1:1:10 column layout. Additionally, updated the sidebar to exclusively show Goddess Seshat and God Thoth with an increased width of 150px and a `<br>` separator.

--- app.py
+++ app.py
@@ -125,21 +125,22 @@ st.markdown(
 )
 
 # Main Header Image and Title
-col1, col2, col3 = st.columns([4, 1, 1])
-with col1:
-    st.title("𓆎𓏏𓈇 Kemet Dock 𓆎𓏏𓈇")
-    st.markdown("### Molecular Docking Portal")
-with col2:
-    st.image("https://upload.wikimedia.org/wikipedia/commons/b/ba/Egypt_Pyramid_Icon.svg", width=80)
-with col3:
+st.title("𓆎𓏏𓈇 Kemet Dock 𓆎𓏏𓈇")
+st.markdown("### Molecular Docking Portal")
+
+# Main Page Doodles - Far Left
+col_cat, col_phar, col_spacer = st.columns([1, 1, 10])
+
+with col_cat:
+    st.image("https://upload.wikimedia.org/wikipedia/commons/a/ae/Egyptian_Cat.svg", width=80)
+
+with col_phar:
     st.image("https://upload.wikimedia.org/wikipedia/commons/a/ab/Pharaoh_in_war.svg", width=80)
 
-# Sidebar Images - 3 Deities
-st.sidebar.image("https://upload.wikimedia.org/wikipedia/commons/7/71/Seshat.svg", width=130)
-st.sidebar.markdown("<div style='height: 10px;'></div>", unsafe_allow_html=True)
-st.sidebar.image("https://upload.wikimedia.org/wikipedia/commons/c/c3/Thoth.svg", width=130)
-st.sidebar.markdown("<div style='height: 10px;'></div>", unsafe_allow_html=True)
-st.sidebar.image("https://upload.wikimedia.org/wikipedia/commons/a/ae/Egyptian_Cat.svg", width=130)
+# Sidebar Images - Deities
+st.sidebar.image("https://upload.wikimedia.org/wikipedia/commons/7/71/Seshat.svg", width=150)
+st.sidebar.markdown("<br>", unsafe_allow_html=True)
+st.sidebar.image("https://upload.wikimedia.org/wikipedia/commons/c/c3/Thoth.svg", width=150)
 
 @st.cache_data
 def load_data():
