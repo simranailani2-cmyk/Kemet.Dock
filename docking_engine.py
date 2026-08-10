5052dce1f9082a498810071dab3f9334ef0e173b
Simran Ailani <simranailani2@gmail.com>
2026-08-10 18:46:20 +0530

    Add seed parameter to docking command

--- docking_engine.py
+++ docking_engine.py
@@ -152,6 +152,7 @@ def run_vina_docking(receptor_pdbqt, ligand_pdbqt, center, dims):
         "--size_z", str(dims[2]),
         "--exhaustiveness", "16",
         "--cpu", "2"
+        "--seed", "42"
     ]
     res = subprocess.run(cmd, capture_output=True, text=True)
     return res.stdout
